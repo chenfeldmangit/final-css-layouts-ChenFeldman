a94ff20a196d21dc0610b221a2113126e45b468f
Chen Feldman <chenfeldmn@gmail.com>
2020-03-22 18:14:54 +0200

    form exercise without bonus

--- scripts.js
+++ scripts.js
@@ -1,3 +1,9 @@
+this.user = {
+    coverImagePath: "https://pbs.twimg.com/profile_banners/1018006119468421120/1560693180/1500x500",
+    profileImagePath: "https://pbs.twimg.com/profile_banners/1018006119468421120/1560693180/1500x500",
+    aboutMe: "Born and raised here so I am part of twitter and will always be"
+};
+
 
 window.onload = () => {
     loadUserData();
@@ -15,14 +21,18 @@ goToHomePage = () => {
 }
 
 loadUserData = () => {
-    fetch('./data/userData.json')
+    /* fetch('./data/userData.json')
     .then(res => res.json())
     .then(data => {
         document.querySelector('#profileContainer .cover-image').style.backgroundImage = `url(${data.coverImagePath})`;
         document.querySelector('#profileContainer .profile-image').style.backgroundImage = `url(${data.coverImagePath})`;
         document.querySelector('#profileContainer .about-me').innerHTML = data.aboutMe;
     })
-    .catch(err => console.error(err));
+    .catch(err => console.error(err)); */
+
+    document.querySelector('#profileContainer .cover-image').style.backgroundImage = `url(${this.user.coverImagePath})`;
+    document.querySelector('#profileContainer .profile-image').style.backgroundImage = `url(${this.user.coverImagePath})`;
+    document.querySelector('#profileContainer .about-me').innerHTML = this.user.aboutMe;
 }
 
 loadTweets = () => {
@@ -30,7 +40,6 @@ loadTweets = () => {
     .then(res => res.json())
     .then(data => {
         data.forEach(item => {
-            debugger;
             addTweetItem(item.userName,item.description);
         }); 
     })
@@ -43,4 +52,30 @@ addTweetItem = (userName, content) => {
     clon.querySelector(".name").innerHTML = userName;
     clon.querySelector(".content").innerHTML = content;
     document.getElementById("newsFeed").appendChild(clon);    
+}
+
+openProfileForm = () => {
+    document.getElementById("overlay").style.display = "flex";
+}
+
+closeProfileForm = (event) => {
+    if (event.target.id === "overlay" || event.target.id === "saveUserData"){
+        document.getElementById("overlay").style.display = "none";
+    }
+}
+
+saveUserData = () => {
+    let userName = document.querySelector("#userName").value;
+    let userDescription = document.querySelector("#userDescription").value;
+
+    // Sample of updating one field. Students will update more than one.
+    this.user.aboutMe = userDescription;
+    
+    loadUserData();
+    closeProfileForm();
+}
+
+// Bonus for the form exercise. Can also use onkeypress
+checkLimit = (charLimit) => {
+        
 }
